277625e0f34dcc15a1b5c9b47a8ae8b8b7444c0c
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-14 02:35:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
-def base_hash  
- base_hash = {}
- base_hash[:railroads] = {}
+def base_hash 
+  base_hash = 
+ {:name => "Dwayne The Rock Johnson"}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	end
