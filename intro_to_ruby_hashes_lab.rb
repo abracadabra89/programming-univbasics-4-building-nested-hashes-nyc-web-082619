69c8daf80404c4e8a91ea1265f0ddd94c6e29495
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-14 02:29:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,12 +5,6 @@ def base_hash
   # keyword "return." Nice and neat.
 	end
 
-def monopoly
-  monopoly = {}
-  monopoly[:railroads] = {}
-end 
-  
-monopoly
 
 def monopoly_with_second_tier
   monopoly = {:railroads => {:pieces => 4, :names => {} ,:rent_in_dollars =>{}}
